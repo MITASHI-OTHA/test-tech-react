320eeb6ce007140dc6b978a5db6c728de3955ea2
MITASHI-OTHA <yahanthony8@gmail.com>
2021-06-25 17:23:55 +0200

    Premier commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import './styles/reset.css';
+import month from './month.json'
+import {useState} from 'react'
+import parse from 'html-react-parser';
+const current = {
+  backgroundColor: '#000000',
+  color: '#fff'
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [months, setMounth] = useState(month)
+
+  const handleMonth = (cur_month)=> {
+    setMounth(months.map(item=> item.month==cur_month?{...item, isCurrent:true}:{...item, isCurrent: false}))
+  }
+
+  let Mois = ()=> months.map((items, id)=> <scroll-container key={id}> <scroll-page id={id} key={id} class="m-p relative" style={items?.isCurrent?current:null} onClick={()=>handleMonth(items.month)}> <a href={`#${id}`}> {items.month} </a>  <div style={{display:items?.isCurrent?'block':'none'}} className="triangle-up"></div> </scroll-page> </scroll-container>  )
+  
+  const ContentText = ()=> {
+    let current = months.find(item=> item.isCurrent)
+
+    return <div style={{flex: 1}}>
+        <h1 className="text-center"> {current.contentTitle} </h1>
+        <p className="text-center mt-5">{parse(current.content)}</p>
     </div>
-  );
+  }
+  
+   return <body>
+        <main>
+          <container>
+            <header>
+              <section>
+                    <div className="d-flex align-items-center justify-content-center relative tabs">
+                      <div className="d-flex align-items-baseline mt w-auto tabs-base">
+                          <Mois/>
+                      </div>
+                    </div>
+                    <article className="d-flex align-items-center justify-content-center article">
+                      <div className="d-flex contentText">
+                        <ContentText/>
+                      </div>
+                    </article>
+              </section>
+            </header>
+          </container>
+        </main>
+                
+     </body>
 }
 
 export default App;
